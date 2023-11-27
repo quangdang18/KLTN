cc8e1509a486f4587edb5dfe78bb26209691a03e
Dang <dang18012001@gmail.com>
2023-11-27 16:42:00 +0700

    change port 82

--- modules/frontend/frontend-alb.tf
+++ modules/frontend/frontend-alb.tf
@@ -17,9 +17,9 @@ resource "aws_security_group" "fe-alb-sg" {
       self             = false
     },
     {
-      description      = "Allow all traffic port 443"
-      from_port        = 443
-      to_port          = 443
+      description      = "Allow all traffic port 82"
+      from_port        = 82
+      to_port          = 82
       protocol         = "tcp"
       cidr_blocks      = [var.internet-cidr]
       ipv6_cidr_blocks = []
