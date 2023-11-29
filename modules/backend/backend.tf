d61581f92b9ba8a4b5f90778a1e906cc3b94fdaa
Dang <dang18012001@gmail.com>
2023-11-29 17:54:50 +0700

    auto_scale

--- modules/backend/backend.tf
+++ modules/backend/backend.tf
@@ -195,3 +195,83 @@ resource "aws_lb_listener" "be_listener" {
 
   depends_on = [aws_lb.be-alb, aws_lb_target_group.backend-tg]
 }
+
+# Launch Template for Backend Instances
+resource "aws_launch_template" "backend" {
+  name_prefix   = "backend-template-"
+  image_id      = "ami-050917ba214f79b9d"  
+  instance_type = "t2.micro"     
+  key_name      = var.ssh-key-name
+  vpc_security_group_ids = [aws_security_group.backend-sg.id]
+}
+
+# Auto Scaling Group for Backend Instances
+resource "aws_autoscaling_group" "backend" {
+  desired_capacity     = 0
+  max_size             = 2  
+  min_size             = 0  
+  vpc_zone_identifier  = var.backend-subnet-ids
+  launch_template {
+    id      = aws_launch_template.backend.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Name"
+    value               = "BE created by Auto Scale"
+    propagate_at_launch = true
+  }
+}
+
+# Auto Scaling Policy for Scaling Up
+resource "aws_autoscaling_policy" "backend_scale_up" {
+  name                   = "backend-scale-up"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.backend.name
+}
+
+# Auto Scaling Policy for Scaling Down
+resource "aws_autoscaling_policy" "backend_scale_down" {
+  name                   = "backend-scale-down"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.backend.name
+}
+
+# CloudWatch Metric Alarm for Scaling Up
+resource "aws_cloudwatch_metric_alarm" "backend_high_cpu" {
+  alarm_name                = "high-cpu-backend"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "60"
+  statistic                 = "Average"
+  threshold                 = "60"
+  alarm_description         = "This metric monitors high CPU utilization for backend"
+  dimensions = {
+    "AutoScalingGroupName" = aws_autoscaling_group.backend.name
+  }
+  alarm_actions             = [aws_autoscaling_policy.backend_scale_up.arn]
+}
+
+# CloudWatch Metric Alarm for Scaling Down
+resource "aws_cloudwatch_metric_alarm" "backend_low_cpu" {
+  alarm_name                = "low-cpu-backend"
+  comparison_operator       = "LessThanThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "60"
+  statistic                 = "Average"
+  threshold                 = "20"
+  alarm_description         = "This metric monitors low CPU utilization for backend"
+  dimensions = {
+    "AutoScalingGroupName" = aws_autoscaling_group.backend.name
+  }
+  alarm_actions             = [aws_autoscaling_policy.backend_scale_down.arn]
+}
+
+
--- modules/frontend/frontend.tf
+++ modules/frontend/frontend.tf
@@ -265,3 +265,86 @@ resource "aws_lb_listener" "fe_listener" {
 
   depends_on = [aws_lb.fe-alb, aws_lb_target_group.frontend-tg]
 }
+
+# Launch Template for Frontend Instances
+resource "aws_launch_template" "frontend" {
+  name_prefix   = "frontend-template-"
+  image_id      = "ami-0b87e05a0dfd8d09c" 
+  instance_type = "t2.micro"
+  key_name      = var.ssh-key-name
+  vpc_security_group_ids = [aws_security_group.frontend-sg.id]
+}
+
+# Auto Scaling Group for Frontend Instances
+resource "aws_autoscaling_group" "frontend" {
+  desired_capacity     = 0
+  max_size             = 2
+  min_size             = 0
+  vpc_zone_identifier  = var.frontend-subnet-ids 
+  launch_template {
+    id      = aws_launch_template.frontend.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "FE created by Auto Scale"
+    propagate_at_launch = true
+  }
+}
+
+# Auto Scaling Policy for Scaling Up
+resource "aws_autoscaling_policy" "frontend_scale_up" {
+  name                   = "frontend-scale-up"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.frontend.name
+}
+
+# Auto Scaling Policy for Scaling Down
+resource "aws_autoscaling_policy" "frontend_scale_down" {
+  name                   = "frontend-scale-down"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.frontend.name
+}
+
+# CloudWatch Metric Alarm for Scaling Up
+resource "aws_cloudwatch_metric_alarm" "frontend_high_request_count" {
+  alarm_name                = "high-request-count"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "RequestCountPerTarget"
+  namespace                 = "AWS/ApplicationELB"
+  period                    = "60"
+  statistic                 = "Sum"
+  threshold                 = "1000"
+  alarm_description         = "This metric monitors high request count for frontend"
+  dimensions = {
+    "LoadBalancer" = aws_lb.fe-alb.arn_suffix
+    "TargetGroup"  = aws_lb_target_group.frontend-tg.arn_suffix
+  }
+  alarm_actions             = [aws_autoscaling_policy.frontend_scale_up.arn]
+}
+
+# CloudWatch Metric Alarm for Scaling Down
+resource "aws_cloudwatch_metric_alarm" "frontend_low_request_count" {
+  alarm_name                = "low-request-count"
+  comparison_operator       = "LessThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "RequestCountPerTarget"
+  namespace                 = "AWS/ApplicationELB"
+  period                    = "300"
+  statistic                 = "Sum"
+  threshold                 = "500"
+  alarm_description         = "This metric monitors low request count for frontend"
+  dimensions = {
+    "LoadBalancer" = aws_lb.fe-alb.arn_suffix
+    "TargetGroup"  = aws_lb_target_group.frontend-tg.arn_suffix
+  }
+  alarm_actions             = [aws_autoscaling_policy.frontend_scale_down.arn]
+}
+
+
